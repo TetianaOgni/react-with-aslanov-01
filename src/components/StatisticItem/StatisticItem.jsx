1ee73c79b07440b1c08abdff570b17a724da7f3a
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-07-08 16:24:58 +0300

    create BlogCard and Statistics

---
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -0,0 +1,23 @@
+import {
+  StatisticBox,
+  StatisticText,
+  StatisticCounter,
+} from './StatisticItem.styled';
+import { IconContext } from "react-icons";
+
+
+export const StatisticItem = ({title, total, icon}) => {
+  return (
+  <>
+  <StatisticBox>
+    <IconContext.Provider value={{ size: "2em", color: "grey", className: "global-class-name" }}>
+    {icon}
+  </IconContext.Provider>
+    <StatisticCounter>{total}</StatisticCounter>
+    <StatisticText>{title}</StatisticText>
+  </StatisticBox>
+  </>
+  )
+  
+};
+
---
+++ src/utils/dateTransform.js
@@ -0,0 +1,6 @@
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+
+export function getDate(dateInfo) {
+    return formatDistanceToNow(new Date(dateInfo)) 
+}
